67449b3708375d26b05666d02a177c303d8d32ce
yrb666666 <45753382+yrb666666@users.noreply.github.com>
2021-11-26 11:16:33 +0800

    Add files via upload

---
+++ lazyfront/pages/test/test.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<view style="color: #007AFF;text-decoration: underline;">
+			哈哈
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+	
+<style>
+	
+</style>
